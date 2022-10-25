b5c7c52adb3399b11c8e5db00845df88bb06aaf7
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-25 17:10:14 +0300

    Chapter 7 - Error handling

--- config.py
+++ config.py
@@ -2,6 +2,15 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['edgerasiimwe@gmail.com']
